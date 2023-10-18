b3ee5309427a06cc8950e76c1a4d5e7dffb5199a
novel <18.novel.18@gmail.com>
2023-10-18 23:17:19 +0300

    add tests

---
+++ src/apps/currency/tests/test_views.py
@@ -0,0 +1,244 @@
+import pytest
+from conftest import get_auth_header
+from rest_framework.test import APIClient
+
+currency_raw = {
+    'name': 'usd',
+    'price_for_buy': 1,
+    'price_for_sale': 20.2
+}
+
+currency_invalid_raw = {
+    'name': 'usd',
+    'price_for_buy': 1.1234,
+    'price_for_sale': 20.223
+}
+
+currency_for_update = {
+    'name': 'dsu',
+    'price_for_buy': 132,
+    'price_for_sale': 202.5
+}
+
+
+@pytest.mark.django_db
+def test_currency_create(admin_user) -> None:  # type: ignore
+    client = APIClient()
+    url = '/api/currencies/'
+    auth_header = get_auth_header(email=admin_user.email, password='admin')
+
+    response = client.post(
+        url,
+        currency_raw,
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 201
+    assert response.data['name'] == currency_raw['name']
+    assert float(response.data['price_for_buy']) == float(currency_raw['price_for_buy'])
+    assert response.data['price_for_buy'].split(".")[1].__len__() == 2
+    assert float(response.data['price_for_sale']) == float(currency_raw['price_for_sale'])
+    assert response.data['price_for_sale'].split(".")[1].__len__() == 2
+
+
+@pytest.mark.django_db
+def test_currency_create_failure(admin_user) -> None:  # type: ignore
+    client = APIClient()
+    url = '/api/currencies/'
+    auth_header = get_auth_header(email=admin_user.email, password='admin')
+
+    response = client.post(
+        url,
+        currency_invalid_raw,
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_currency_create_access_denied(user1) -> None:  # type: ignore
+    client = APIClient()
+    url = '/api/currencies/'
+    auth_header = get_auth_header(email=user1.email, password='1')
+
+    response = client.post(
+        url,
+        currency_raw,
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 403
+
+    response = client.post(
+        url,
+        currency_raw,
+        format='json'
+    )
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_currency_list(user1) -> None:  # type: ignore
+    client = APIClient()
+    url = '/api/currencies/'
+    auth_header = get_auth_header(email=user1.email, password='1')
+
+    response = client.get(
+        url,
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 200
+    assert isinstance(response.data, list)
+
+
+@pytest.mark.django_db
+def test_currency_list_access_denied() -> None:  # type: ignore
+    client = APIClient()
+    url = '/api/currencies/'
+
+    response = client.get(
+        url,
+        format='json'
+    )
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_currency_retrieve(currency_usd, user1) -> None:  # type: ignore
+    client = APIClient()
+    url = f'/api/currencies/{currency_usd.id}/'
+    auth_header = get_auth_header(email=user1.email, password='1')
+
+    response = client.get(
+        url,
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 200
+    assert response.data['name'] == currency_usd.name
+    assert int(response.data['id']) == currency_usd.id
+    assert float(response.data['price_for_sale']) == currency_usd.price_for_sale
+    assert float(response.data['price_for_buy']) == currency_usd.price_for_buy
+
+
+@pytest.mark.django_db
+def test_currency_retrieve_access_denied(currency_usd) -> None:  # type: ignore
+    client = APIClient()
+    url = f'/api/currencies/{currency_usd.id}/'
+
+    response = client.get(
+        url,
+        format='json'
+    )
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_currency_put(currency_usd, admin_user) -> None:  # type: ignore
+    client = APIClient()
+    url = f'/api/currencies/{currency_usd.id}/'
+    auth_header = get_auth_header(email=admin_user.email, password='admin')
+
+    response = client.put(
+        url,
+        currency_for_update,
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 200
+    assert response.data['name'] == currency_for_update['name']
+    assert float(response.data['price_for_sale']) == currency_for_update['price_for_sale']
+    assert float(response.data['price_for_buy']) == currency_for_update['price_for_buy']
+
+
+@pytest.mark.django_db
+def test_currency_put_access_denied(currency_usd, user1) -> None:  # type: ignore
+    client = APIClient()
+    url = f'/api/currencies/{currency_usd.id}/'
+    auth_header = get_auth_header(email=user1.email, password='1')
+
+    response = client.put(
+        url,
+        currency_for_update,
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_currency_patch(currency_usd, admin_user) -> None:  # type: ignore
+    client = APIClient()
+    url = f'/api/currencies/{currency_usd.id}/'
+    auth_header = get_auth_header(email=admin_user.email, password='admin')
+
+    response = client.patch(
+        url,
+        {'name': currency_for_update['name']},
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 200
+    assert response.data['name'] == currency_for_update['name']
+    assert float(response.data['price_for_sale']) == currency_usd.price_for_sale
+    assert float(response.data['price_for_buy']) == currency_usd.price_for_buy
+
+
+@pytest.mark.django_db
+def test_currency_patch_access_denied(currency_usd, user1) -> None:  # type: ignore
+    client = APIClient()
+    url = f'/api/currencies/{currency_usd.id}/'
+    auth_header = get_auth_header(email=user1.email, password='1')
+
+    response = client.patch(
+        url,
+        {'name': currency_for_update['name']},
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_currency_delete(currency_usd, admin_user) -> None:  # type: ignore
+    client = APIClient()
+    url = f'/api/currencies/{currency_usd.id}/'
+    auth_header = get_auth_header(email=admin_user.email, password='admin')
+
+    response = client.delete(
+        url,
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 204
+    assert response.data is None
+
+    response = client.get(
+        url,
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 404
+
+
+@pytest.mark.django_db
+def test_currency_delete_access_denied(currency_usd, user1) -> None:  # type: ignore
+    client = APIClient()
+    url = f'/api/currencies/{currency_usd.id}/'
+    auth_header = get_auth_header(email=user1.email, password='1')
+
+    response = client.delete(
+        url,
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 403
+
+    response = client.get(
+        url,
+        headers=auth_header,
+        format='json'
+    )
+    assert response.status_code == 200
--- src/conftest.py
+++ src/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from django.contrib.auth import get_user_model
 
+from apps.authentication.services import AuthenticationService
+from apps.currency.models import Currency
+
 
 @pytest.fixture
 def user1(db):
@@ -10,3 +13,32 @@ def user1(db):
         username='1',
     )
     return user
+
+
+@pytest.fixture
+def admin_user(db):
+    admin = get_user_model().objects.create_superuser(
+        email='admin@admin.com',
+        password='admin',
+    )
+    return admin
+
+
+@pytest.fixture
+def currency_usd(db):
+    currency = Currency.objects.create(
+        name='usd',
+        price_for_buy=1,
+        price_for_sale=20.2,
+    )
+    return currency
+
+
+def get_auth_header(email, password):
+    auth_header = {
+        'Authorization': 'Bearer ' + AuthenticationService.generate_jwt(
+            email=email,
+            password=password,
+        )
+    }
+    return auth_header
